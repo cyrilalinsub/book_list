680dcb98f6889feb76fa920a798baf3fd27afef2
cyrilgalinsub@utexas.edu <cyrilgalinsub@utexas.edu>
2022-01-09 01:09:42 -0600

    Props

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const firstBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/91vnzZO5yPL._AC_UL200_SR200,200_.jpg',
+  title: 'The Very Hungry Caterpillar',
+  author: 'Eric Carle',
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const secondBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL200_SR200,200_.jpg',
+  title: 'I Love You to the Moon and Back',
+  author: 'Amelia Hepworth',
+}
+
+function BookList() {
+  return(
+    <section className='booklist'>
+      <Book 
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book 
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
+    </section>
+  );
+};
+
+const Book = (props) => {
+  return (
+    <article className='book'>
+      <img src={props.img} alt='' />
+      <h1>{props.title}</h1>
+      <h4>{props.author.toUpperCase()}</h4>
+    </article>
+  );
+};
+
+ReactDom.render(<BookList />, document.getElementById('root'));
